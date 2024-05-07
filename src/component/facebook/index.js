395353a16c00614fd7526e99fe29158523268e0d
sarjansog16 <sarjansog16@gmail.com>
2024-05-07 23:50:36 +0545

    remaingbugfixes

--- src/component/facebook/index.js
+++ src/component/facebook/index.js
@@ -3,8 +3,8 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 
 const Facebook = () => {
     return (
-      <FacebookProvider appId="123456789" chatSupport>
-        <CustomChat pageId="123456789" minimized={false}/>
+      <FacebookProvider appId="1343240006351367" chatSupport>
+        <CustomChat pageId="107401024287642" minimized={true}/>
       </FacebookProvider>    
     );
   

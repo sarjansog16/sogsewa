444c1fd71593e298178d02956d2f7a3047c8afa3
sarjansog16 <sarjansog16@gmail.com>
2024-05-07 23:27:26 +0545

    messangerChatbot

--- src/component/facebook/index.js
+++ src/component/facebook/index.js
@@ -2,12 +2,11 @@ import React, { Component} from 'react';
 import { FacebookProvider, CustomChat } from 'react-facebook';
 
 const Facebook = () => {
-    return(
-        <FacebookProvider appId="2025869981129330" chatSupport>
-        <CustomChat pageId="107401024287642" minimized={false}/>
+    return (
+      <FacebookProvider appId="123456789" chatSupport>
+        <CustomChat pageId="123456789" minimized={false}/>
       </FacebookProvider>    
-    )
-
+    );
+  
 }
-
 export default Facebook;
